7d24a2b7ae757cdfe77e666ce83619463c35e9ec
xtuyaowu <toyaowu@gmail.com>
2021-05-29 12:37:01 +0800

    for test

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@ int main() {
   app()
       .setLogPath("./")
       .setLogLevel(trantor::Logger::kWarn)
-      .addListener("0.0.0.0", 80)
+      .addListener("0.0.0.0", 8080)
       .setThreadNum(16)
-      .enableRunAsDaemon()
+      //.enableRunAsDaemon()
       .run();
 }
